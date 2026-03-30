41882451adb957cdb8bc90cae3b1bf595de036a4
Baptiste RICHARD <143808981+TronnoxUwU@users.noreply.github.com>
2026-03-30 09:43:57 +0200

    classification par temps

--- src/data/clusterisation.py
+++ src/data/clusterisation.py
@@ -10,9 +10,6 @@ def classifier_nappe_fluctuation(df, col="niveau_nappe_eau"):
     variation_moy = variations.abs().mean()
     variabilite = variations.std()
 
-    # pente moyenne (tendance globale)
-    x = np.arange(len(niveau))
-
     # indice de dynamique
     indice = variation_moy + variabilite
 
@@ -28,3 +25,72 @@ def classifier_nappe_fluctuation(df, col="niveau_nappe_eau"):
         "indice_dynamique": indice,
         "type_nappe": type_nappe
     }
+
+def classifier_par_duree(df, col="niveau_nappe_eau"):
+    df = df = df.copy().dropna(subset=[col])
+    
+    grouped = df.groupby("code_bss")
+    
+    resultats = {
+        ">30 ans": 0,
+        ">20 ans": 0,
+        ">10 ans": 0,
+        "<10 ans": 0
+    }
+    
+    for _, group in grouped:
+        duree = (group["time"].max() - group["time"].min()).days / 365.25
+        
+        if duree > 30:
+            resultats[">30 ans"] += 1
+        elif duree > 20:
+            resultats[">20 ans"] += 1
+        elif duree > 10:
+            resultats[">10 ans"] += 1
+        else:
+            resultats["<10 ans"] += 1
+
+    return resultats
+
+def classifier_par_consecutif(df, col="niveau_nappe_eau"):
+    df = df.copy().dropna(subset=[col])
+    
+    grouped = df.groupby("code_bss")
+    
+    resultats = {
+        ">30 ans": 0,
+        ">20 ans": 0,
+        ">10 ans": 0,
+        "<10 ans": 0
+    }
+    
+    for _, group in grouped:
+        dates = group["time"].sort_values().drop_duplicates()
+        
+        if len(dates) == 0:
+            continue
+        
+        max_streak = 1
+        current_streak = 1
+        
+        for i in range(1, len(dates)):
+            diff = (dates.iloc[i].year - dates.iloc[i-1].year) * 12 + \
+                   (dates.iloc[i].month - dates.iloc[i-1].month)
+            
+            if diff == 1:
+                current_streak += 1
+                max_streak = max(max_streak, current_streak)
+            else:
+                current_streak = 1
+
+        if max_streak > 30:
+            resultats[">30 ans"] += 1
+        elif max_streak > 20:
+            resultats[">20 ans"] += 1
+        elif max_streak > 10:
+            resultats[">10 ans"] += 1
+        else:
+            resultats["<10 ans"] += 1
+
+    
+    return resultats
